fb3b752bcbff5d90abd514a0b444c9847e94b05b
YalamarthiJyotsna <126884678+YalamarthiJyotsna@users.noreply.github.com>
2023-03-08 16:05:42 +0530

    Add files via upload

---
+++ ArrayDemo1.java
@@ -0,0 +1,41 @@
+package array;
+
+
+public class ArrayDemo1 
+  {
+	
+	
+
+	public static void main(String[] args) 
+	{
+      
+		int arr[] = new int[5];
+         
+		arr[0]= 10;
+		
+		System.out.println(arr[0]);
+        
+		arr[1]= 20;
+		
+		System.out.println(arr[1]);
+        
+		arr[2]= 30;
+		
+		System.out.println(arr[2]);
+        
+	//	arr[3]= 40;
+		
+		System.out.println(arr[3]);
+        
+		//arr[4]= 50;
+		
+		System.out.println(arr[4]);
+		
+        
+	//	arr[5]= 10;
+		
+	//	System.out.println(arr[5]);//run time error
+		
+	}
+
+}
